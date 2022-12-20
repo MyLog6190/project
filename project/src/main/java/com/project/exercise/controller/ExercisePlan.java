ed6a7ee71f3b05796c0679f71cb91eea30aaad4f
user <user@Aspire-A515-44>
2022-12-20 09:40:38 +0900

    commit

--- project/src/main/java/com/project/exercise/controller/ExercisePlan.java
+++ project/src/main/java/com/project/exercise/controller/ExercisePlan.java
@@ -1,15 +1,24 @@
 package com.project.exercise.controller;
 
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import com.project.exercise.dto.ExerciseLibDTO;
+import com.project.exercise.dto.ExercisePlanDTO;
 import com.project.exercise.service.ExerciseLibService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,22 +34,31 @@ public class ExercisePlan {
 	private final ExerciseLibService exerciseLibService ;
 	
 	@GetMapping("/plan")
-	public String hello(Model model) {
+	public String calendar(Model model) {
+		return "/exercise-plan";
+	}
+	
+	@PostMapping("/exercise-list")
+	public void ExerciseList(HttpServletResponse response) {
 		List<ExerciseLibDTO> list = null;
 		try {
 			list = exerciseLibService.getAllExerciseLib();
-			model.addAttribute("elist", list);
-			log.info(model);
 		} catch(Exception e) {
 			log.error(e.getMessage());
 			log.info("라이브러리 로드 과정에서 문제 발생");
 		}
-		return "/exercise-plan";
+		try {
+			new Gson().toJson(list,response.getWriter());
+		} catch (JsonIOException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	@GetMapping("/b")
-	public String hello2(@RequestParam("code") String code) {
-		System.out.println(code);
-		return "/exercise-plan";
+	@PostMapping("/select-exercise")
+	public void SelectExercise(@RequestBody Map<String, Object> pr ,HttpServletResponse response) {
+		List<ExercisePlanDTO> list = null;
+		
 	}
 }
